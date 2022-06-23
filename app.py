73e38b7ccde9451f93a2b04541d6b63683e1a843
FoocoJun <85068289+FoocoJun@users.noreply.github.com>
2022-06-23 12:52:04 +0900

    뉴스 빼고 다

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def team_temp(teamtitle):
 
     # 팀에 관한 db 불러오기
     team = db.teams.find_one({'name': teamtitle})
-    if team:
+    if team: # 주소가 올바른 팀 이름이면(데이터베이스에 있는 팀 이름이면!)
         # 팀 경기결과 불러오기
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -204,27 +204,20 @@ def team_temp(teamtitle):
         return jsonify({'msg': '올바르지 않은 접근방식입니다.'})
 
 
-@app.route("/news/read", methods=["GET"])
-def news_get():
-    # 뉴스 내용 표시
-    all_article = list(db.articles.find({}, {'_id': False}))
-    all_users = list(db.users.find({}, {'_id': False}))
-    return jsonify({'all_article': all_article, 'all_users': all_users})
-
 
 @app.route("/team/read", methods=["GET"])
 def article_get():
     # 게시판 내용 표시
-    all_article = list(db.articles.find({}, {'_id': False}))
+    all_articles = list(db.articles.find({}, {'_id': False}))
     all_users = list(db.users.find({}, {'_id': False}))
-    return jsonify({'all_article': all_article, 'all_users': all_users})
+    return jsonify({'all_article': all_articles, 'all_users': all_users})
 
 
 @app.route("/team/write", methods=["POST"])
 def ariticle_post():
-    #쿠키에서 로그인 토큰을 받아온다.
+    # 쿠키에서 로그인 토큰을 받아온다.
     token_receive = request.cookies.get('mytoken')
-    try: #로그인되어 있으면
+    try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         userNickname = payload['id']
 
@@ -251,7 +244,7 @@ def ariticle_post():
         db.articles.insert_one(doc)
         return jsonify({'msg': '등록되었습니다.'})
     except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return jsonify({'msg': "로그인을 해야합니다."})
+        return jsonify({'msg': "글 작성은 로그인을 해야합니다."})
 
 
 
@@ -280,14 +273,14 @@ def article_like():
                 isliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['liked']
                 # 좋아요 수(like) = 좋아요 명단-싫어요 명단
                 db.articles.update_one({'num': int(number_receive)}, {'$set': {'like': len(isliked) - len(isdisliked)}})
-                return jsonify({'msg': '좋아요를 했습니다.'})
+                return ('', 204) #브라우저에 아무런 응답도 하지 않는 방법
 
             elif isliked.count(userNickname) == 1:  # 좋아요 명단에 이름이 있으면
                 # 좋아요 명단에서 이름(userNickname) pull
                 db.articles.update_one({'num': int(number_receive)}, {'$pull': {'liked': userNickname}})
                 isliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['liked']
                 db.articles.update_one({'num': int(number_receive)}, {'$set': {'like': len(isliked) - len(isdisliked)}})
-                return jsonify({'msg': '좋아요를 취소했습니다.'})
+                return ('', 204)
 
         elif isdisliked.count(userNickname) == 1:  # 싫어요 명단에 이름(userNickname)이 있고
             if isliked.count(userNickname) == 0:  # 좋아요 명단에 이름(userNickname)이 없으면
@@ -299,9 +292,9 @@ def article_like():
                 isliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['liked']
                 isdisliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['disliked']
                 db.articles.update_one({'num': int(number_receive)}, {'$set': {'like': len(isliked) - len(isdisliked)}})
-                return jsonify({'msg': '좋아요를 했습니다.'})
+                return jsonify('', 204)
     except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home", msg="로그인을 해야합니다."))
+        return jsonify({'msg': "로그인을 해야합니다."})
 
 
 # 좋아요 / 싫어요 라디오 버튼식 구현 (싫어요)
@@ -323,12 +316,12 @@ def article_dislike():
                 db.articles.update_one({'num': int(number_receive)}, {'$push': {'disliked': userNickname}})
                 isdisliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['disliked']
                 db.articles.update_one({'num': int(number_receive)}, {'$set': {'like': len(isliked) - len(isdisliked)}})
-                return jsonify({'msg': '싫어요를 했습니다.'})
+                return ('', 204)
             elif isdisliked.count(userNickname) == 1:  # 싫어요 명단에 이름(userNickname)이 있으면
                 db.articles.update_one({'num': int(number_receive)}, {'$pull': {'disliked': userNickname}})
                 isdisliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['disliked']
                 db.articles.update_one({'num': int(number_receive)}, {'$set': {'like': len(isliked) - len(isdisliked)}})
-                return jsonify({'msg': '싫어요를 취소했습니다.'})
+                return ('', 204)
 
         elif isliked.count(userNickname) == 1:  # 좋아요 명단에 이름(userNickname)이 있고
             if isdisliked.count(userNickname) == 0:  # 싫어요 명단에 이름이 없으면
@@ -339,9 +332,9 @@ def article_dislike():
                 isliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['liked']
                 isdisliked = db.articles.find_one({'num': int(number_receive)}, {'_id': False})['disliked']
                 db.articles.update_one({'num': int(number_receive)}, {'$set': {'like': len(isliked) - len(isdisliked)}})
-                return jsonify({'msg': '싫어요를 했습니다.'})
+                return ('', 204)
     except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home", msg="로그인을 해야합니다."))
+        return jsonify({'msg': "로그인을 해야합니다."})
 
 
 # admin /시즌 말 팀 목록 초기화 크롤링
